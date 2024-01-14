ff358e96fd24f0c4ada14a7241d0871a83d7e8d0
오준석 <y22zzp@naver.com>
2024-01-14 19:53:17 +0900

    Java 입문 프로그램 실행 및 주석 처리

---
+++ src/CommentJava.java
@@ -0,0 +1,8 @@
+public class CommentJava {
+    public static void main(String[] args) { // psvm. public statc void main 단축키.
+        System.out.println("hello Java1"); // sout이 단축키
+        // 일반 주석. 한 줄 주석
+        /* 범위 주석. 여러 줄 주석  */
+
+    }
+}
---
+++ src/HelloJava.java
@@ -0,0 +1,5 @@
+public class HelloJava { // 파일명과 클래스 이름이 같아야 한다. {} 블록을 사용해서 시작과 끝을 나타냄
+    public static void main(String[] args){
+        System.out.println("hello java"); // 값을 콘솔에 출력하는 기능
+    } // main() 메서드의 범위 끝
+} // HelloJava 클래스 범위 끝
