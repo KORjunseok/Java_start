eb0f91ed7533a2ef7bd91ea00a546c3891a32acd
오준석 <y22zzp@naver.com>
2024-01-15 20:47:01 +0900

    Java 입문 변수

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long tenBillion = 10000000000L;
+        boolean trust = true;
+        System.out.println(tenBillion);
+        System.out.println(trust);
+
+    }
+}
